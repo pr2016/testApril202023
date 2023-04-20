b49e988185923cc7db49c1c106d1a2f5406c9c08
Preethi Mohan <prmo2016@gmail.com>
2023-04-20 14:39:11 -0700

    first commit

---
+++ Test5Git/src/test5CalledTrees/TestClass1.java
@@ -0,0 +1,9 @@
+package test5CalledTrees;
+
+public class TestClass1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi Test1");
+	}
+}
---
+++ Test5Git/src/test5CalledTrees/TestClass2.java
@@ -0,0 +1,10 @@
+package test5CalledTrees;
+
+public class TestClass2 
+{	
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi Test1");
+	}
+
+}
